c6faebf80b1b041ec496826dc0a5e3983cd888b3
Pranjal Sharma <pranjalsharmaps5.com>
2025-03-12 00:28:41 +0530

    aaj ki raat

--- ArrayQuestion@6/Recursion.java
+++ ArrayQuestion@6/Recursion.java
@@ -1,15 +1,31 @@
 public class Recursion {
+//    static int sum= 0;
+    static int f( /*int i,*/ int n) {
+//         tyr//
+    if(n<0)
+        System.out.println(n);
 
-    static void f(int i, int n) {
-        if (i > n)
-            return;   // this is baseline to stop the function
-        f(i + 1, n);
-        System.out.println(i);
+    return f(n-1);
 
+//        here we have done with single parameter
+//        if (n == 1)
+//            return 1;
+//
+////      return n + f(n-1);
+//
+//      return n * f(n-1); //  this is for factorial program
+
+        // here we have done with single sorry double  parameter
+//        if (i <1) {
+//            System.out.print(sum);
+//            return;   // this is baseline to stop the function
+//        }
+//        f(i - 1, sum+i);
 
     }
 
     public static void main(String[] args) {
-        f(1, 4);
+//        f(8);
+        System.out.println(f(7));
     }
 }
